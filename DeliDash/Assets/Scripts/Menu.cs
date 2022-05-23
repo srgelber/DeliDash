d3bd9c69ea8e15de63e47cafd4062263515b97e2
Joe Monteagudo <74952125+jmonteag@users.noreply.github.com>
2022-05-23 13:55:21 -0700

    Demo Fixed
    Made Demo 3D/2D

--- DeliDash/Assets/Scripts/Menu.cs
+++ DeliDash/Assets/Scripts/Menu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
     public void PlayGame(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Demo1");
     }
 
     public void QuitGame(){
